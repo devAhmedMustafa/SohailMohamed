d34a91985eacb441831143381c89df84711b1132
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2023-09-14 15:45:13 +0300

    Adding My Website hyperlink

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,12 @@ const Footer = ()=>{
 
     return (
         <div className="bg-[#1b1b1b] flex justify-center py-4 font-oswald text-xl gap-4 border-t-[1px] border-[#414141]">
-            <span>Developed By: Ahmed Mustafa</span>
+            <div className="bg-[#ffffff17] px-4 py-1 rounded-full">Developed By: Ahmed Mustafa</div>
             <button onClick={()=> OpenLink("https://www.instagram.com/ahmedmustafa.dev/")}>
                 <i className="fa-brands fa-instagram"></i>
+            </button>
+            <button onClick={()=> OpenLink("https://www.ahmedmustafa.vercel.app")}>
+                <i className="fa-solid fa-laptop-code"></i>
             </button>
         </div>
     )

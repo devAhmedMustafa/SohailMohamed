d231bb58ede945baab97f7e80bb200073d8f9210
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2023-09-14 15:30:59 +0300

    Initail Commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -25,7 +25,7 @@ const Intro = ()=>{
     
             <div className="relative w-fit flex flex-col gap-6 items-center">
 
-                <h1 className="text-5xl md:text-7xl flex justify-center gap-2 2xl:flex-col 2xl:text-9xl w-full overflow-hidden">
+                <h1 className="text-5xl md:text-7xl flex justify-center gap-2 2xl:flex-col 2xl:text-9xl w-full overflow-hidden name">
                     <span className="sohail translate-x-96">Sohail </span>
                     <span className="mohamed translate-x-96">Mohamed</span>
                 </h1>
